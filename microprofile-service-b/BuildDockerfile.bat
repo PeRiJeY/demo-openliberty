9ff7996aa23841c08a9af8df3514c0b2720e1ea6
Juan German Arriaza Posada <juan.german.arriaza@gmail.com>
2019-11-29 11:03:38 +0100

    Agrupped projects

---
+++ microprofile-service-b/BuildDockerfile.bat
@@ -0,0 +1,2 @@
+docker build -t gcr.io/buoyant-ceiling-256716/service-b:0.0.9 .
+docker push gcr.io/buoyant-ceiling-256716/service-b:0.0.9
--- service-a/BuildDockerfile.bat
+++
@@ -1,2 +0,0 @@
-docker build -t gcr.io/buoyant-ceiling-256716/service-a:0.0.9 .
-docker push gcr.io/buoyant-ceiling-256716/service-a:0.0.9
